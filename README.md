5c5cfa2a5deb07e97565a670ad43950e6eac9798
igor-pimenta-araujo <igor.araujo0147@gmail.com>
2023-07-28 20:21:21 -0300

    Primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # gptapi
+instalar dependencias do requirements.txt
+```bash
+rodar comando: uvicorn chat:app --reload
---
+++ chat.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import openai
+import os
+
+# Configurando a chave de API OpenAI (substitua pela sua) pegar do .env
+openai.api_key = 
+
+# Definindo o modelo FastAPI
+app = FastAPI()
+
+# Criando um modelo Pydantic para as entradas da API
+class Query(BaseModel):
+    prompt: str
+    max_tokens: int = 100
+
+# Definindo a rota de API para responder perguntas usando GPT-3
+@app.post("/ask")
+async def ask_gpt3(query: Query):
+    response = openai.Completion.create(
+      engine="text-davinci-002", # você pode usar outros modelos também, depende da sua necessidade
+      prompt=query.prompt,
+      max_tokens=query.max_tokens
+    )
+    return response.choices[0].text.strip()
